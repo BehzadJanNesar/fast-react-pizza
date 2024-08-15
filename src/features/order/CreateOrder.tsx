a10795dd62701595a0d8b734a95620a961dccb8a
BehzadJanNesar <behzadjannesar.2001@gmail.com>
2024-08-15 17:53:52 +0330

    add some tailwind styles

--- src/features/order/CreateOrder.tsx
+++ src/features/order/CreateOrder.tsx
@@ -1,51 +1,53 @@
-import { useState, ChangeEvent, FormEvent } from "react";
+import { useState, ChangeEvent } from "react";
+import { Form, redirect } from "react-router-dom";
+import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
 // const isValidPhone = (str: string): boolean =>
 //    /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(str);
 
 // Define a type for the cart items
-// type CartItem = {
-//    pizzaId: number;
-//    name: string;
-//    quantity: number;
-//    unitPrice: number;
-//    totalPrice: number;
-// };
+type CartItem = {
+   pizzaId: number;
+   name: string;
+   quantity: number;
+   unitPrice: number;
+   totalPrice: number;
+};
 
-// const fakeCart: CartItem[] = [
-//    {
-//       pizzaId: 12,
-//       name: "Mediterranean",
-//       quantity: 2,
-//       unitPrice: 16,
-//       totalPrice: 32,
-//    },
-//    {
-//       pizzaId: 6,
-//       name: "Vegetale",
-//       quantity: 1,
-//       unitPrice: 13,
-//       totalPrice: 13,
-//    },
-//    {
-//       pizzaId: 11,
-//       name: "Spinach and Mushroom",
-//       quantity: 1,
-//       unitPrice: 15,
-//       totalPrice: 15,
-//    },
-// ];
+const fakeCart: CartItem[] = [
+   {
+      pizzaId: 12,
+      name: "Mediterranean",
+      quantity: 2,
+      unitPrice: 16,
+      totalPrice: 32,
+   },
+   {
+      pizzaId: 6,
+      name: "Vegetale",
+      quantity: 1,
+      unitPrice: 13,
+      totalPrice: 13,
+   },
+   {
+      pizzaId: 11,
+      name: "Spinach and Mushroom",
+      quantity: 1,
+      unitPrice: 15,
+      totalPrice: 15,
+   },
+];
 
 function CreateOrder(): JSX.Element {
    const [withPriority, setWithPriority] = useState<boolean>(false);
-   // const cart: CartItem[] = fakeCart;
+   const cart: CartItem[] = fakeCart;
 
    // Handle the form submission (if necessary)
-   const handleFormSubmit = (event: FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-      // Add form submission logic here
-   };
+   // const handleFormSubmit = (event: FormEvent<HTMLFormElement>) => {
+   //    event.preventDefault();
+   //    // Add form submission logic here
+   // };
 
    // Handle changes to the checkbox
    const handleCheckboxChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -56,7 +58,8 @@ function CreateOrder(): JSX.Element {
       <div>
          <h2>Ready to order? Let's go!</h2>
 
-         <form onSubmit={handleFormSubmit}>
+         {/* <Form method="POST"> action="/order/new" */}
+         <Form method="POST">
             <div>
                <label htmlFor="customer">First Name</label>
                <input type="text" name="customer" id="customer" required />
@@ -88,11 +91,27 @@ function CreateOrder(): JSX.Element {
             </div>
 
             <div>
+               <input type="hidden" name="cart" value={JSON.stringify(cart)} />
                <button type="submit">Order now</button>
             </div>
-         </form>
+         </Form>
       </div>
    );
+}
+
+export async function action({ request }) {
+   const formData = await request.formData();
+   const data = Object.fromEntries(formData);
+
+   const order = {
+      ...data,
+      cart: JSON.parse(data.cart),
+      priority: data.priority === "on",
+   };
+
+   const newOrder = await createOrder(order);
+
+   return redirect(`/order/${newOrder.id}`);
 }
 
 export default CreateOrder;
--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -1,14 +1,15 @@
-import { Link } from "react-router-dom";
-import SearchOrder from "../features/order/SearchOrder";
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
 
 function Header() {
-   return (
-      <header>
-         <Link to="/">/Fast react pizza Co.</Link>
-         <SearchOrder />
-         <p>Behzad</p>
-      </header>
-   );
+  return (
+    <header className="border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
+      <Link to="/" className="tracking-widest">
+        Fast react pizza Co.
+      </Link>
+      <SearchOrder />
+    </header>
+  );
 }
 
 export default Header;
